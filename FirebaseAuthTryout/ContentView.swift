c7796922b2f767ac24117d51f02829d10fde4879
Sarah.Berbuir <livine.gmks@gmail.com>
2022-10-05 16:55:10 +0200

    Initial Commit

---
+++ FirebaseAuthTryout/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseAuthTryout
+//
+//  Created by Sarah Berbuir on 05.10.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FirebaseAuthTryout/FirebaseAuthTryoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthTryoutApp.swift
+//  FirebaseAuthTryout
+//
+//  Created by Sarah Berbuir on 05.10.22.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthTryoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
